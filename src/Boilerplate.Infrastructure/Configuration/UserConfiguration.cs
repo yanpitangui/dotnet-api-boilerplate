c788e37d0175ac54f99c22717093747845fbc5ef
Yan Pitangui <yanpitangui@gmail.com>
2022-01-09 11:47:37 -0300

    Updated userconfiguration to make admin id static

--- src/Boilerplate.Infrastructure/Configuration/UserConfiguration.cs
+++ src/Boilerplate.Infrastructure/Configuration/UserConfiguration.cs
@@ -18,14 +18,14 @@ namespace Boilerplate.Infrastructure.Configuration
             builder.HasData(
                 new User
                 {
-                    Id = Guid.NewGuid(),
+                    Id = new Guid("687d9fd5-2752-4a96-93d5-0f33a49913c6"),
                     Email = "admin@boilerplate.com",
                     Role = "Admin",
                     Password = BC.HashPassword("adminpassword")
                 },
                 new User
                 {
-                    Id = Guid.NewGuid(),
+                    Id = new Guid("6648c89f-e894-42bb-94f0-8fd1059c86b4"),
                     Email = "user@boilerplate.com",
                     Role = "User",
                     Password = BC.HashPassword("userpassword")
