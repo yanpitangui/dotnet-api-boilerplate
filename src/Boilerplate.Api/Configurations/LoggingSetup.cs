47d2d80653aee0f9635e9b1a6bfffdb38f20a6d4
Yan Pitangui <yanpitangui@gmail.com>
2023-03-14 21:51:33 -0300

    UP: Modernize use of serilog to read from configuration

--- src/Boilerplate.Api/Configurations/LoggingSetup.cs
+++ src/Boilerplate.Api/Configurations/LoggingSetup.cs
@@ -1,44 +1,18 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.SystemConsole.Themes;
-using System;
-
 namespace Boilerplate.Api.Configurations;
 
 public static class LoggingSetup
 {
     public static IHostBuilder UseLoggingSetup(this IHostBuilder host, IConfiguration configuration)
     {
-        host.UseSerilog((_, services, lc) =>
+        host.UseSerilog((_, _, lc) =>
         {
-            lc.ConfigureBaseLogging(configuration, services);
+            lc.ReadFrom.Configuration(configuration);
         });
 
         return host;
     }
 
-}
-
-public static class LoggerExtension
-{
-    public static LoggerConfiguration ConfigureBaseLogging(
-        this LoggerConfiguration loggerConfiguration, IConfiguration configuration, IServiceProvider services
-    )
-    {
-        loggerConfiguration
-            .MinimumLevel.Information()
-            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
-            .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", LogEventLevel.Warning)
-            .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Infrastructure", LogEventLevel.Warning)
-            .WriteTo.Async(a =>
-            {
-                a.Console(outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}",
-                    theme: AnsiConsoleTheme.Code);
-            })
-            .Enrich.FromLogContext();
-
-        return loggerConfiguration;
-    }
 }
